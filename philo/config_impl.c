9d1092c0efba3a873a2690db548054840617c75e
Tuomas <tuomas.jaasalo@alceon.fi>
2023-07-05 17:21:26 +0300

    Limited config ms values to 1 <> intmax

--- philo/config_impl.c
+++ philo/config_impl.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 14:50:41 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/06/30 05:20:17 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/07/05 17:15:31 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	config_time_to_die(t_config *self, const char *arg)
 	if (!self->is_valid)
 		return ;
 	errstr = NULL;
-	self->time_to_die = ft_strtonum(arg, 0, int_max(), &errstr);
+	self->time_to_die = ft_strtonum(arg, 1, int_max(), &errstr);
 	if (!errstr)
 		return ;
 	ft_putstr_fd("Invalid time to die!\n", STDERR_FILENO);
@@ -48,7 +48,7 @@ void	config_time_to_eat(t_config *self, const char *arg)
 	if (!self->is_valid)
 		return ;
 	errstr = NULL;
-	self->time_to_eat = ft_strtonum(arg, 0, int_max(), &errstr);
+	self->time_to_eat = ft_strtonum(arg, 1, int_max(), &errstr);
 	if (!errstr)
 		return ;
 	ft_putstr_fd("Invalid time to eat!\n", STDERR_FILENO);
@@ -62,7 +62,7 @@ void	config_time_to_sleep(t_config *self, const char *arg)
 	if (!self->is_valid)
 		return ;
 	errstr = NULL;
-	self->time_to_sleep = ft_strtonum(arg, 0, int_max(), &errstr);
+	self->time_to_sleep = ft_strtonum(arg, 1, int_max(), &errstr);
 	if (!errstr)
 		return ;
 	ft_putstr_fd("Invalid time to sleep!\n", STDERR_FILENO);
